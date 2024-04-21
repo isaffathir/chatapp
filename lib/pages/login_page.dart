7496e9a18895ef0a0d807133e9bd88777a605bfd
isaffathir <isaffathir@outlook.com>
2024-04-21 21:45:18 +0700

    Add padding to make it responsive

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -21,70 +21,73 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: SizedBox(
-            width: 400,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Logo
-                Icon(
-                  CupertinoIcons.chat_bubble_2,
-                  size: 120,
-                  color: Colors.grey[800],
-                ),
-                // Welcome message
-                Text(
-                  'M I N I M A L',
-                  style: TextStyle(
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: SizedBox(
+              width: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Logo
+                  Icon(
+                    CupertinoIcons.chat_bubble_2,
+                    size: 120,
                     color: Colors.grey[800],
-                    fontSize: 20,
-                    fontFamily: 'Poppins',
                   ),
-                ),
-
-                const SizedBox(
-                  height: 30,
-                ),
-
-                // email
-                MyTextField(
-                  controller: emailController,
-                  hintText: "Email",
-                ),
-
-                const SizedBox(
-                  height: 10,
-                ),
-                // password
-                MyTextField(
-                  controller: passwordController,
-                  hintText: "Password",
-                  obscureText: obscure,
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      // Based on passwordVisible state choose the icon
-                      obscure ? Icons.visibility : Icons.visibility_off,
-                      color: Colors.grey,
+                  // Welcome message
+                  Text(
+                    'M I N I M A L',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontSize: 20,
+                      fontFamily: 'Poppins',
                     ),
-                    onPressed: () {
-                      setState(() {
-                        obscure = !obscure;
-                      });
-                    },
                   ),
-                ),
 
-                const SizedBox(
-                  height: 30,
-                ),
+                  const SizedBox(
+                    height: 30,
+                  ),
 
-                // login button
-                const MyButton(
-                  text: "Log in",
-                ),
+                  // email
+                  MyTextField(
+                    controller: emailController,
+                    hintText: "Email",
+                  ),
 
-                // register
-              ],
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  // password
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: "Password",
+                    obscureText: obscure,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        // Based on passwordVisible state choose the icon
+                        obscure ? Icons.visibility : Icons.visibility_off,
+                        color: Colors.grey,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          obscure = !obscure;
+                        });
+                      },
+                    ),
+                  ),
+
+                  const SizedBox(
+                    height: 30,
+                  ),
+
+                  // login button
+                  const MyButton(
+                    text: "Log in",
+                  ),
+
+                  // register
+                ],
+              ),
             ),
           ),
         ),
